638924e4d8cb7a4bd753a76a932068441d243c99
Marc Radziwill <marc.radzi@gmail.com>
2019-12-18 09:43:24 +0100

    refactor: 💡 puppeteer packages

---
+++ src/index.js
@@ -0,0 +1,59 @@
+const puppeteer = require('puppeteer');
+
+const carbon = async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.tracing.start();
+    await page.goto('https://www.websitecarbon.com/');
+    await page.type('#wgd-cc-url', 'https://marcradziwill.com');
+    await page.click('#js-new-test-button');
+  
+    const percentSelector =
+      '.media > .media__content > .report-summary__heading > .highlight > .js-countup';
+    const co2Selector = '.section #js-emission-count';
+    const hostingSelector = '.report-hosting .media__content';
+    const statsSelector = '.stat .media__content';
+  
+    await page.waitForSelector(percentSelector);
+  
+    const percent = await page.$eval(percentSelector, (el) =>
+      el.getAttribute('data-count'),
+    );
+    await page.waitForSelector(co2Selector);
+    const emissionCount = await page.$eval(co2Selector, (el) =>
+      el.getAttribute('data-count'),
+    );
+    const hostingReport = await page.$eval(hostingSelector, (el) =>
+      el.innerHTML.replace(/(\r\n|\n|\r)/gm, '').trim(),
+    );
+    
+    const statsList = await page.$$eval(statsSelector, (stats) => {
+      return stats.map((stat) => {
+        return stat.innerHTML.replace(/(\r\n|\n|\r)/gm, '').trim();
+      });
+    });
+  
+    await page.tracing.stop();
+    await browser.close();
+  
+    const obj = {
+      CARBON_PERCENT: percent,
+      hosting: hostingReport,
+      co2: emissionCount,
+      stats: statsList,
+    };
+
+    console.log(obj);
+  
+    // res.status(200).send(obj);
+    
+  } catch (error) {
+    console.log(error);
+    // res.status(500).send(error.message);
+  }
+}
+carbon()
+module.exports = {
+  carbon: carbon
+}
